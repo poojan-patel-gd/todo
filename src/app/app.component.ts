8315eea55f78eca116bed761c32bad88cd29372c
poojan-patel-gd <poojan.patel@ghanshyamdigital.com>
2023-01-24 10:00:20 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
   todoList: any[] = [];
-  localList = JSON.parse(localStorage.getItem('user-Todo') || '[]');
   SubmitTask(data: string) {
     this.todoList.push({ id: this.todoList.length, name: data });
-    localStorage.setItem('user-Todo', JSON.stringify(this.todoList));
-    console.log(this.todoList);
   }
   removeTodo(id: number) {
     this.todoList = this.todoList.filter((item) => item.id !== id);
